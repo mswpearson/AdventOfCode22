502a4d073f34e20c8e15ddf2f20d1e4ce9a9321a
Matthew Pearson <matthew.pearson@waterstons.com>
2022-12-01 13:50:57 +0000

    Add project files.

---
+++ AdventOfCode22-Day1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+string inputText = File.ReadAllText("puzzleInput.txt");
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
+var splitInput = inputText.Split("\r\n\r\n");
+
+var totalElfCalories = splitInput.Select(input => input.Split("\r\n").Sum(i => int.Parse(i)));
+
+var top3Calories = totalElfCalories.OrderByDescending(calories => calories).Take(3);
+var sum = top3Calories.Sum();
+stopwatch.Stop();
+
+Console.WriteLine(sum); 
+Console.WriteLine(stopwatch.Elapsed);
