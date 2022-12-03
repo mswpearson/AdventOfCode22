35250ea3845350464dddd14b65df1de68e154d8a
Matthew Pearson <matthew.pearson@waterstons.com>
2022-12-03 18:07:49 +0000

    day3 p1

---
+++ AdventOfCode22.Day3/Program.cs
@@ -0,0 +1,31 @@
+﻿string inputText = File.ReadAllText("puzzleInput.txt");
+var bags = inputText.Split("\r\n");
+var compartmentTuples = bags.Select(bag => (bag.Substring(0, bag.Length/2), bag.Substring(bag.Length/2, bag.Length/2)));
+
+var total = compartmentTuples.Sum(compartmentTuple => GetCompartmentTupleTotal(compartmentTuple));
+
+Console.WriteLine(total);
+
+int GetCompartmentTupleTotal((string compartment1, string compartment2) compartmentTuple)
+{
+    char duplicateChar = 'A';
+    foreach(char @char in compartmentTuple.compartment1)
+    {
+        if (compartmentTuple.compartment2.Contains(@char))
+        {
+            duplicateChar = @char;
+            break;
+        }
+    }
+
+    int charVal;
+    if (char.IsUpper(duplicateChar)) 
+    {
+        charVal = duplicateChar - 38;
+    }
+    else
+    {
+        charVal = duplicateChar - 96;
+    }
+    return charVal;
+}
