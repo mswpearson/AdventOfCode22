02300c6f4bce6f8b23f3a4273e82b5a1fd227291
Matthew Pearson <matthew.pearson@waterstons.com>
2022-12-06 09:19:26 +0000

    part 1

---
+++ AdventOfCode22.Day6/Program.cs
@@ -0,0 +1,17 @@
+﻿string inputText = File.ReadAllText("puzzleInput.txt");
+var chars = inputText.ToCharArray();
+var charQueue = new Queue<char>();
+
+for (int i = 0; i < chars.Length; i++)
+{
+    charQueue.Enqueue(chars[i]);
+    if (charQueue.Count() == 5)
+    {
+        charQueue.Dequeue();
+        if (charQueue.Distinct().Count() == 4)
+        {
+            Console.WriteLine(i+1);
+            break;
+        }
+    }
+}
