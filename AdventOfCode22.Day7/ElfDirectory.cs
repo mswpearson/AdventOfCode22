13568ffb1e862d6eb84f53981f44b071fbc29532
Matthew Pearson <matthew.pearson@waterstons.com>
2022-12-07 14:49:07 +0000

    part 1

---
+++ AdventOfCode22.Day7/ElfDirectory.cs
@@ -0,0 +1,9 @@
+﻿namespace AdventOfCode22.Day7
+{
+    public class ElfDirectory
+    {
+        public string Path { get; set; } = string.Empty;
+        public List<int> FileSizes { get; set; } = new List<int>();
+        public List<string> SubDirectories { get; set; } = new List<string>();
+    }
+}
--- AdventOfCode22.Day7/Program.cs
+++ AdventOfCode22.Day7/Program.cs
@@ -1,1 +1,68 @@
-﻿string inputText = File.ReadAllText("puzzleInput.txt");
+﻿using AdventOfCode22.Day7;
+
+string inputText = File.ReadAllText("puzzleInput.txt");
+var directoryChanges = inputText.Split("$ cd ").Where(dc => !string.IsNullOrWhiteSpace(dc.Trim())).Select(dc => dc.Trim());
+
+string currentDirectory = "";
+List<ElfDirectory> allDirectories = new List<ElfDirectory>();
+
+foreach (var directoryChange in directoryChanges)
+{
+    var directory = new ElfDirectory();
+    if (!directoryChange.StartsWith(".."))
+    {
+        var splitDirectoryChange = directoryChange.Split("$ ls");
+
+        var directoryName = splitDirectoryChange[0].Trim();
+        if (string.IsNullOrWhiteSpace(currentDirectory) && directoryName == "/")
+        {
+            currentDirectory = "/";
+        }
+        else if (currentDirectory.EndsWith("/"))
+        {
+            currentDirectory += directoryName;
+        }
+        else
+        {
+            currentDirectory += $"/{directoryName}";
+        }
+        directory.Path = currentDirectory;
+
+        var insideDirectory = splitDirectoryChange[1].Split("\r\n").Where(x => !string.IsNullOrWhiteSpace(x.Trim()));
+        foreach (var fileOrDir in insideDirectory)
+        {
+            if (fileOrDir.StartsWith("dir"))
+            {
+                if (currentDirectory.EndsWith("/"))
+                {
+                    directory.SubDirectories.Add(currentDirectory + fileOrDir.Split(" ")[1]);
+                }
+                else
+                {
+                    directory.SubDirectories.Add(currentDirectory + "/" + fileOrDir.Split(" ")[1]);
+                }
+            }
+            else
+            {
+                directory.FileSizes.Add(int.Parse(fileOrDir.Split(" ")[0]));
+            }
+        }
+        allDirectories.Add(directory);
+    }
+    else
+    {
+        var lastSlashIndex = currentDirectory.LastIndexOf('/');
+        currentDirectory = currentDirectory.Substring(0, lastSlashIndex);
+    }
+}
+
+int GetSizeOfDirectory(string fullDirectoryPath)
+{
+    var directory = allDirectories.Single(dir => dir.Path == fullDirectoryPath);
+
+    return directory.FileSizes.Sum() + directory.SubDirectories.Sum(sd => GetSizeOfDirectory(sd));
+}
+
+var allDirectorySizes = allDirectories.Select(dir => GetSizeOfDirectory(dir.Path));
+
+Console.WriteLine(allDirectorySizes.Where(dSize => dSize <= 100_000).Sum());
