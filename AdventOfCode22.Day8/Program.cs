f6b5bbfe5f305e357f86fab2cb0c6e0547f0048b
Matthew Pearson <matthew.pearson@waterstons.com>
2022-12-08 21:17:00 +0000

    part 2

--- AdventOfCode22.Day8/Program.cs
+++ AdventOfCode22.Day8/Program.cs
@@ -2,36 +2,52 @@
 
 var rows = inputText.Split("\r\n").Select(row => row.ToCharArray().Select(@char => int.Parse(@char.ToString())).ToArray()).ToArray();
 
-var visibleTrees = 0;
+var scenicScores = new List<int>();
 
 for (int row=0; row < rows.Length; row++)
 {
     for (int column = 0; column < rows[row].Length; column++)
     {
-        if (IsVisible(rows[row][column], row, column)) visibleTrees ++;
+        scenicScores.Add(GetScenicScore(rows[row][column], row, column));
     }
 }
 
-Console.WriteLine(visibleTrees);
+Console.WriteLine(scenicScores.Max());
 
-bool IsVisible(int treeHeight, int row, int col)
+int GetScenicScore(int treeHeight, int row, int col)
 {
-    // visible from left
     var toTheLeft = rows[row].Take(col);
-    if (toTheLeft.All(tree => tree < treeHeight)) return true;
+    var leftVD = GetViewingDistance(treeHeight, toTheLeft.Reverse().ToArray());
 
-    // visible from right
     var toTheRight = rows[row].Skip(col+1);
-    if (toTheRight.All(tree => tree < treeHeight)) return true;
+    var rightVD = GetViewingDistance(treeHeight, toTheRight.ToArray());
 
-    // visible from top
     var column = rows.Select(row => row[col]);
     var above = column.Take(row);
-    if (above.All(tree => tree < treeHeight)) return true;
+    var aboveVD = GetViewingDistance(treeHeight, above.Reverse().ToArray());
 
-    // visible from bottom
     var below = column.Skip(row+1);
-    if (below.All(tree => tree < treeHeight)) return true;
+    var belowVD = GetViewingDistance(treeHeight, below.ToArray());
 
-    return false;
+    return leftVD * rightVD * aboveVD * belowVD;
+}
+
+int GetViewingDistance(int treeHeight, int[] treeHeightsLookingFromTree)
+{
+    var nonBlocking = new List<int>();
+
+    foreach (int tree in treeHeightsLookingFromTree)
+    {
+        if (tree < treeHeight)
+        {
+            nonBlocking.Add(tree);
+        }
+        else
+        {
+            nonBlocking.Add(tree);
+            break;
+        }
+    }
+
+    return nonBlocking.Count();
 }
