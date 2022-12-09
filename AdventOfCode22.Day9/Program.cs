df98fd136a89604ed2a45cc9f2f3d44f29c82b45
Matthew Pearson <matthew.pearson@waterstons.com>
2022-12-09 22:44:12 +0000

    PART 1 + 2 FINALLY HOLY MOLY

--- AdventOfCode22.Day9/Program.cs
+++ AdventOfCode22.Day9/Program.cs
@@ -3,161 +3,129 @@ string[] instructions = inputText.Split("\r\n");
 
 var positionsCoveredByT = new List<(int x, int y)>();
 
-var positions = Enumerable.Repeat<(int x, int y)>((0, 0), 10).ToArray();
+// Part 1
+//var rope = Enumerable.Repeat<(int x, int y)>((0, 0), 2).ToArray();
+
+// Part 2
+// If using the visualiser function, set the initial values to something like (25,25) so that it doesn't go out of bounds
+var rope = Enumerable.Repeat<(int x, int y)>((0, 0), 10).ToArray();
 
 foreach (var instruction in instructions)
 {
-    var splitInstruction = instruction.Split(" ");
+    var splitInstruction = instruction.Split(' ');
     var direction = splitInstruction[0];
     var distance = int.Parse(splitInstruction[1]);
 
-    for (int moved = 0; moved < distance; moved++)
+    for (int i=0; i < distance; i++)
     {
-        for (int position = 1; position < positions.Length; position++)
-        {
-            ref var hPos = ref positions[position - 1];
-            ref var tPos = ref positions[position];
-            if (position == 1)
-            {
-                direction = splitInstruction[0];
-            }
-            switch (direction)
-            {
-                case "U":
-                    if (position == 1) 
-                    {
-                        hPos = (hPos.x, hPos.y + 1);
-                    }
-                    if (!TailIsTouching(hPos, tPos))
-                    {
-                        if (hPos == (tPos.x - 1, tPos.y + 2))
-                        {
-                            if ((splitInstruction[0] == "U"))
-                                direction = "L";
-                            tPos = (tPos.x - 1, tPos.y + 1);
-                        }
-                        else if (hPos == (tPos.x, tPos.y + 2))
-                        {
-                            tPos = (tPos.x, tPos.y + 1);
-                        }
-                        else
-                        {
-                            if ((splitInstruction[0] == "U"))
-                                direction = "R";
-                            tPos = (tPos.x + 1, tPos.y + 1);
-                        }
-                    }
-                    break;
-                case "D":
-                    if (position == 1)
-                    {
-                        hPos = (hPos.x, hPos.y - 1);
-                    }
-                    if (!TailIsTouching(hPos, tPos))
-                    {
-                        if (hPos == (tPos.x - 1, tPos.y - 2))
-                        {
-                            if ((splitInstruction[0] == "D")) 
-                                direction = "L";
-                            tPos = (tPos.x - 1, tPos.y - 1);
-                        }
-                        else if (hPos == (tPos.x, tPos.y - 2))
-                        {
-                            tPos = (tPos.x, tPos.y - 1);
-                        }
-                        else
-                        {
-                            tPos = (tPos.x + 1, tPos.y - 1);
-                        }
-                    }
-                    break;
-                case "L":
-                    if (position == 1)
-                    {
-                        hPos = (hPos.x - 1, hPos.y);
-                    }
-                    if (!TailIsTouching(hPos, tPos))
-                    {
-                        if (hPos == (tPos.x - 2, tPos.y - 1))
-                        {
-                            if ((splitInstruction[0] == "L")) 
-                                direction = "D";
-                            tPos = (tPos.x - 1, tPos.y - 1);
-                        }
-                        else if (hPos == (tPos.x - 2, tPos.y))
-                        {
-                            tPos = (tPos.x - 1, tPos.y);
-                        }
-                        else
-                        {
-                            if ((splitInstruction[0] == "L"))
-                                direction = "U";
-                            tPos = (tPos.x - 1, tPos.y + 1);
-                        }
-                    }
-                    break;
-                case "R":
-                    if (position == 1)
-                    {
-                        hPos = (hPos.x + 1, hPos.y);
-                    }
-                    if (!TailIsTouching(hPos, tPos))
-                    {
-                        if (hPos == (tPos.x + 2, tPos.y - 1))
-                        {
-                            if ((splitInstruction[0] == "R"))
-                                direction = "D";
-                            tPos = (tPos.x + 1, tPos.y - 1);
-                        }
-                        else if (hPos == (tPos.x + 2, tPos.y))
-                        {
-                            tPos = (tPos.x + 1, tPos.y);
-                        }
-                        else
-                        {
-                            if ((splitInstruction[0] == "R"))
-                                direction = "U";
-                            tPos = (tPos.x + 1, tPos.y + 1);
-                        }
-                    }
-                    break;
-                default:
-                    throw new InvalidOperationException();
-            }
-        }
-        DrawPositions(instruction, positions);
+        MoveHead(direction);
 
-        positionsCoveredByT.Add(positions[9]);
+        for (int position=1; position < rope.Length; position++)
+        {
+            Follow(position);
+        }
+
+        positionsCoveredByT.Add(rope[rope.Length - 1]);
     }
 }
 
 Console.WriteLine(positionsCoveredByT.Distinct().Count());
 
-bool TailIsTouching((int x, int y) hPos, (int x, int y) tPos)
+void MoveHead(string direction)
+{
+    switch (direction)
+    {
+        case "U":
+            rope[0].y += 1;
+            break;
+        case "D":
+            rope[0].y -= 1;
+            break;
+        case "L":
+            rope[0].x -= 1;
+            break;
+        case "R":
+            rope[0].x += 1;
+            break;
+    }
+}
+
+void Follow(int followerIndex)
+{
+    ref var toFollow = ref rope[followerIndex - 1];
+    ref var follower = ref rope[followerIndex];
+
+    if (!IsTouching(follower, toFollow))
+    {
+        string direction = "";
+        if (toFollow.x > follower.x) direction += "R";
+        else if (toFollow.x < follower.x) direction += "L";
+        if (toFollow.y > follower.y) direction += "U";
+        else if (toFollow.y < follower.y) direction += "D";
+
+        switch (direction)
+        {
+            case "R":
+                follower.x += 1;
+                break;
+            case "L":
+                follower.x -= 1;
+                break;
+            case "U":
+                follower.y += 1;
+                break;
+            case "D":
+                follower.y -= 1;
+                break;
+            case "RU":
+                follower.x += 1;
+                follower.y += 1;
+                break;
+            case "RD":
+                follower.x += 1;
+                follower.y -= 1;
+                break;
+            case "LU":
+                follower.x -= 1;
+                follower.y += 1;
+                break;
+            case "LD":
+                follower.x -= 1;
+                follower.y -= 1;
+                break;
+            default:
+                throw new Exception();
+        }
+    }
+}
+
+bool IsTouching((int x, int y) @this, (int x, int y) that)
 {
     var touchingPositions = new List<(int x, int y)>
     {
-        (tPos.x - 1, tPos.y + 1),
-        (tPos.x, tPos.y + 1),
-        (tPos.x + 1, tPos.y + 1),
-        (tPos.x - 1, tPos.y),
-        tPos,
-        (tPos.x + 1, tPos.y),
-        (tPos.x - 1, tPos.y - 1),
-        (tPos.x, tPos.y - 1),
-        (tPos.x + 1, tPos.y - 1)
+        (that.x - 1, that.y + 1),
+        (that.x, that.y + 1),
+        (that.x + 1, that.y + 1),
+        (that.x - 1, that.y),
+        that,
+        (that.x + 1, that.y),
+        (that.x - 1, that.y - 1),
+        (that.x, that.y - 1),
+        (that.x + 1, that.y - 1)
     };
 
-    return touchingPositions.Contains(hPos);
+    return touchingPositions.Contains(@this);
 }
 
-void DrawPositions(string instruction, IEnumerable<(int x, int y)> positions)
+// You can call this at various times through the solve to display the rope in a rudimentary manner
+// Can end up throwing out of bounds very easily
+void DrawPositions(string instruction)
 {
     var line = Enumerable.Repeat(" ", 50).Select(cell => " ").ToArray();
     var grid = Enumerable.Repeat(line.ToArray(), 50).Select(line => line.Clone() as string[]).ToArray();
 
     int positionIndex = 0;
-    foreach (var position in positions)
+    foreach (var position in rope)
     {
         if (string.IsNullOrWhiteSpace(grid[position.y][position.x]))
         {
