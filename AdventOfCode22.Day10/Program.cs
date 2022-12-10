7a8e326d90d42ff56c7eda7d9bd83944b826c0a0
Matthew Pearson <matthew.pearson@waterstons.com>
2022-12-10 11:50:04 +0000

    PART 1

---
+++ AdventOfCode22.Day10/Program.cs
@@ -0,0 +1,39 @@
+﻿string inputText = File.ReadAllText("puzzleInput.txt");
+string[] instructions = inputText.Split("\r\n");
+
+int cycle = 0;
+int X = 1;
+
+var signalStrengthsCollector = new List<int>();
+
+foreach(var instruction in instructions)
+{
+    ExecuteInstruction(instruction);
+}
+
+void ExecuteInstruction(string instruction)
+{
+    if (instruction == "noop")
+    {
+        cycle += 1;
+        CollectIfNeed();
+    }
+    else
+    {
+        cycle += 1;
+        CollectIfNeed();
+        cycle += 1;
+        CollectIfNeed();
+        X += int.Parse(instruction.Split(' ')[1]);
+    }
+}
+
+void CollectIfNeed()
+{
+    if (new int[] { 20, 60, 100, 140, 180, 220 }.Contains(cycle))
+    {
+        signalStrengthsCollector.Add(cycle * X);
+    }
+}
+
+Console.WriteLine(signalStrengthsCollector.Sum());
