b5914155df8f1001b2c96301f0cf160bb6aefa6b
Matthew Pearson <matthew.pearson@waterstons.com>
2022-12-11 12:51:43 +0000

    Part 1, quite happy with this OOP approach today

---
+++ AdventOfCode22.Day11/Monkey.cs
@@ -0,0 +1,67 @@
+﻿namespace AdventOfCode22.Day11
+{
+    public class Monkey
+    {
+        private readonly string Operator;
+        private readonly string LeftOfOperator;
+        private readonly string RightOfOperator;
+        private readonly int DivisibleBy;
+        private readonly int TrueMonkeyIndex;
+        private readonly int FalseMonkeyIndex;
+
+        public int ItemsInspected { get; set; }
+
+        public Queue<int> Items { get; set; } = new Queue<int>();
+
+        public Monkey(string monkeyInput)
+        {
+            var splitInput = monkeyInput.Split("\r\n");
+
+            var splitStartingItems = splitInput[1].Trim().Split(' ');
+            for (int i=2; i < splitStartingItems.Length; i++)
+            {
+                Items.Enqueue(int.Parse(splitStartingItems[i].Split(',')[0]));
+            }
+
+            var splitOperation = splitInput[2].Trim().Split(' ');
+            LeftOfOperator = splitOperation[3];
+            Operator = splitOperation[4];
+            RightOfOperator = splitOperation[5];
+
+            var splitTest = splitInput[3].Trim().Split(' ');
+            DivisibleBy = int.Parse(splitTest[3]);
+
+            var splitTrue = splitInput[4].Trim().Split(" ");
+            TrueMonkeyIndex = int.Parse(splitTrue[5]);
+
+            var splitFalse = splitInput[5].Trim().Split(" ");
+            FalseMonkeyIndex = int.Parse(splitFalse[5]);
+        }
+
+        public (int newMonkeyIndex, int worryLevel) InspectNext()
+        {
+            int item = Items.Dequeue();
+            item = Operation(item);
+            item /= 3;
+            var newMonkeyIndex = Test(item);
+
+            ItemsInspected++;
+
+            return (newMonkeyIndex, item);
+        }
+
+        private int Operation(int oldWorryLevel)
+        {
+            var left = LeftOfOperator == "old" ? oldWorryLevel : int.Parse(LeftOfOperator);
+            var right = RightOfOperator == "old" ? oldWorryLevel : int.Parse(RightOfOperator);
+
+            if (Operator == "+") return left + right;
+            else return left * right;
+        }
+
+        private int Test(int worryLevel)
+        {
+            return worryLevel % DivisibleBy == 0 ? TrueMonkeyIndex : FalseMonkeyIndex;
+        }
+    }
+}
---
+++ AdventOfCode22.Day11/Program.cs
@@ -0,0 +1,23 @@
+﻿using AdventOfCode22.Day11;
+
+string inputText = File.ReadAllText("puzzleInput.txt");
+string[] monkeyInputs = inputText.Split("Monkey ");
+
+Monkey[] monkeys = monkeyInputs.Where(mi => !string.IsNullOrWhiteSpace(mi)).Select(mi => new Monkey(mi)).ToArray();
+
+int rounds = 20;
+
+for (int round = 0; round < rounds; round++)
+{
+    foreach (var monkey in monkeys)
+    {
+        while (monkey.Items.Any())
+        {
+            var item = monkey.InspectNext();
+            monkeys[item.newMonkeyIndex].Items.Enqueue(item.worryLevel);
+        }
+    }
+}
+
+var top2 = monkeys.Select(m => m.ItemsInspected).OrderByDescending(ii => ii).Take(2).ToArray();
+Console.WriteLine(top2[0] * top2[1]);
